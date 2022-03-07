d84c576006307f896c9bcc3a01d1182d4cf8e8d4
me.ankit88@gmail.com <me.ankit88@gmail.com>
2022-03-07 12:16:28 -0500

    fibbonacci

--- src/recusionsLearn/Recursion.java
+++ src/recusionsLearn/Recursion.java
@@ -5,6 +5,9 @@ public class Recursion {
     public static void main(String[] args) {
         System.out.println(Recursion.reverseString("hello"));
         System.out.println(Recursion.isPalindrome("racecar"));
+        System.out.println(Recursion.recursiveSummation(5));
+        System.out.println(Recursion.binarySearch(new int[]{1, 2, 3, 4, 5, 6}, 1, 6, 5));
+        Recursion.printFibonacci(10);
     }
     public static String reverseString(String input) {
 
@@ -86,11 +89,62 @@ p - works really well with recursive structures like trees and graphs
 
         return false;
     }
-    /*
 
-     */
+    // Sum Of all natural numbers
 
+    public static int recursiveSummation(int inputnumber) {
 
+        if (inputnumber <= 1) {
+            return inputnumber;
+        }
 
+        return inputnumber + recursiveSummation(inputnumber -1);
+    }
+
+    // Divide and Conquer
+    // Binary Search
+
+//    sorted list of numbers - key point they are sorted
+
+    public static int binarySearch(int[] A, int left, int right, int x) {
+
+        if(left > right) {
+            return -1;
+        }
+
+        int mid =  (left + right) /2;
+
+        if (x == A[mid]) {
+            return  mid;
+        }
+
+        if (x< A[mid]) {
+            return binarySearch(A, left, mid-1, x);
+        }
+
+        return binarySearch(A, mid+1, right, x);
+    }
+
+    // Fibonacci
+
+    public static void printFibonacci(int N) {
+        int num1 = 0, num2 = 1;
+
+        int counter = 0;
+
+        // Iterate till counter is N
+        while (counter < N) {
+
+            // Print the number
+            System.out.print(num1 + " ");
+
+            // Swap
+            int num3 = num2 + num1;
+            num1 = num2;
+            num2 = num3;
+            counter = counter + 1;
+        }
+
+    }
 
 }
